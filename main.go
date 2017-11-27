ac948da1e1e57c3c42da9e2eda02c96daf96fd53
tamal <tamal@appscode.com>
2017-11-27 10:51:46 -0800

    added-all

--- main.go
+++ main.go
@@ -4,17 +4,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 
 	"github.com/ghodss/yaml"
 	"github.com/spf13/cobra"
 	apps "k8s.io/api/apps/v1beta1"
+	"k8s.io/apimachinery/pkg/util/jsonmergepatch"
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
 )
+
+var patchTypes = []string{"json", "merge", "strategic"}
 
 func main() {
 	var (
 		src string
 		dst string
+		t   string
 	)
 	cmd := &cobra.Command{
 		Use:               "diff",
@@ -39,7 +44,13 @@ func main() {
 				log.Fatalln(err)
 			}
 
-			patch, err := strategicpatch.CreateTwoWayMergePatch(srcJson, dstJson, apps.Deployment{})
+			var patch []byte
+			switch t {
+			case "strategic":
+				patch, err = strategicpatch.CreateTwoWayMergePatch(srcJson, dstJson, apps.Deployment{})
+			case "merge":
+				patch, err = jsonmergepatch.CreateThreeWayJSONMergePatch(srcJson, dstJson, srcJson)
+			}
 			if err != nil {
 				log.Fatalln(err)
 			}
@@ -48,6 +59,7 @@ func main() {
 	}
 	cmd.Flags().StringVarP(&src, "src", "s", src, "source file")
 	cmd.Flags().StringVarP(&dst, "dst", "d", dst, "desired file")
+	cmd.Flags().StringVarP(&t, "type", "t", "strategic", fmt.Sprintf("The type of patch being provided; one of %v", strings.Join(patchTypes, ",")))
 
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
